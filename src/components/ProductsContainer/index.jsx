4a62f748f46ac502b745b60894a76957c5f59b37
Lucca Martinelli <lucnjm@gmail.com>
2025-08-01 21:16:18 -0300

    add files of products container

---
+++ src/components/ProductsContainer/index.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react'
+import './index.css'
+
+function ProductsContainer({className}){
+    const [sucessProducts, setSucessProducts] = useState([]);;
+
+    useEffect(() => {
+        fetch('http://localhost:3000/sucess-products')
+        .then(res => res.json())
+        .then(data => {
+            setSucessProducts(data);
+        })
+
+    })
+
+    return(
+        <div className={`container-products ${className}`}>
+            <h1 className="title-products">Nossos Produtos de Sucesso</h1>
+            <div className='container-sucess-products'>
+                {sucessProducts?.map((product, index) => (
+                    <span className='container-product' key={index}>
+                        <img className='img-product' src={product.url} alt={product.name} />
+                        <h2>{product.name}</h2>
+                        <p>Valor: {product.price}</p>
+                        <button className='button-book' type='button'>Encomendar</button>
+                    </span>
+                ))}
+            </div>
+            <div className='container-link'>
+                <a href='/products' className='link-products'>Veja mais produtos</a>
+            </div>
+        </div>
+    )
+}
+
+export default ProductsContainer
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,14 +3,16 @@ import ApresentationContainer from "../../components/ApresentationContainer"
 import NavBar from "../../components/NavBar"
 
 import './index.css'
+import ProductsContainer from "../../components/ProductsContainer";
 
 function Home() {
-    const [menuHidden, setMenuHidden] = useState(true);
+    const [menuHidden, setMenuHidden] = useState(false);
 
     return(
         <div className={`container-home ${!menuHidden ? 'closed' : 'open'}`}>
         <NavBar menuHidden={menuHidden} onToggleMenu={() => setMenuHidden(!menuHidden)} className="navbar"/>
         <ApresentationContainer className="apresentation-container"/>
+        <ProductsContainer className="products-container"/>
         </div>
     )
 }
